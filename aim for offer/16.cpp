a3e70e85b94882346403edf3837675ea360b50f8
Yuan Li <liyuan989@gmail.com>
2015-05-01 17:13:07 +0800

    Format

--- aim for offer/16.cpp
+++ aim for offer/16.cpp
@@ -7,88 +7,88 @@
 
 struct Node
 {
-	int    value;
-	Node*  next;
+    int    value;
+    Node*  next;
 
-	Node()
-		: value(0), next(NULL)
-	{
-	}
+    Node()
+        : value(0), next(NULL)
+    {
+    }
 };
 
 Node* initList(int* array, int size)
 {
-	if (array == NULL || size <= 0)
-	{
-		return NULL;
-	}
-	Node* head = new Node;
-	Node* p = head;
-	for (int i = 0; i < size; ++i)
-	{
-		p->next = new Node;
-		p->next->value = array[i];
-		p = p->next;
-	}
-	return head;
+    if (array == NULL || size <= 0)
+    {
+        return NULL;
+    }
+    Node* head = new Node;
+    Node* p = head;
+    for (int i = 0; i < size; ++i)
+    {
+        p->next = new Node;
+        p->next->value = array[i];
+        p = p->next;
+    }
+    return head;
 }
 
 void reverseList(Node* head)
 {
-	if (head == NULL || head->next == NULL || head->next->next == NULL)
-	{
-		return;
-	}
-	Node* prev = head->next;
-	Node* middle = head->next->next;
-	prev->next = NULL;
-	while (middle)
-	{
-		Node* end = middle->next;
-	    middle->next = prev;
-	    prev = middle;
-	    middle = end;
-	}
-	head->next = prev;
+    if (head == NULL || head->next == NULL || head->next->next == NULL)
+    {
+        return;
+    }
+    Node* prev = head->next;
+    Node* middle = head->next->next;
+    prev->next = NULL;
+    while (middle)
+    {
+        Node* end = middle->next;
+        middle->next = prev;
+        prev = middle;
+        middle = end;
+    }
+    head->next = prev;
 }
 
 int main(int argc, char* argv[])
 {
-	{
-		int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		Node* head = initList(array, 10);
-		reverseList(NULL);
-		reverseList(head);
-		for (Node* p = head; p->next; p = p->next)
-		{
-			printf("%d ", p->next->value);
-		}
-		printf("\n");
-	}
+    {
+        int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Node* head = initList(array, 10);
+        reverseList(NULL);
+        reverseList(head);
+        for (Node* p = head; p->next; p = p->next)
+        {
+            printf("%d ", p->next->value);
+        }
+        printf("\n");
+    }
 
-	{
-		int array[] = {1, 2};
-		Node* head = initList(array, 2);
-		reverseList(NULL);
-		reverseList(head);
-		for (Node* p = head; p->next; p = p->next)
-		{
-			printf("%d ", p->next->value);
-		}
-		printf("\n");
-	}
+    {
+        int array[] = {1, 2};
+        Node* head = initList(array, 2);
+        reverseList(NULL);
+        reverseList(head);
+        for (Node* p = head; p->next; p = p->next)
+        {
+            printf("%d ", p->next->value);
+        }
+        printf("\n");
+    }
 
-	{
-		int array[] = {1};
-		Node* head = initList(array, 1);
-		reverseList(NULL);
-		reverseList(head);
-		for (Node* p = head; p->next; p = p->next)
-		{
-			printf("%d ", p->next->value);
-		}
-		printf("\n");
-	}
+    {
+        int array[] = {1};
+        Node* head = initList(array, 1);
+        reverseList(NULL);
+        reverseList(head);
+        for (Node* p = head; p->next; p = p->next)
+        {
+            printf("%d ", p->next->value);
+        }
+        printf("\n");
+    }
 
-	return 0;
+    return 0;
 }
--- aim for offer/17.cpp
+++ aim for offer/17.cpp
@@ -8,119 +8,119 @@
 
 struct Node
 {
-	int    value;
-	Node*  next;
+    int    value;
+    Node*  next;
 
-	Node()
-		: value(0), next(NULL)
-	{
-	}
+    Node()
+        : value(0), next(NULL)
+    {
+    }
 };
 
 Node* initList(int* array, int size)
 {
-	if (array == NULL || size <= 0)
-	{
-		return NULL;
-	}
-	Node* head = new Node;
-	Node* p = head;
-	for (int i = 0; i < size; ++i)
-	{
-		p->next = new Node;
-		p->next->value = array[i];
-		p = p->next;
-	}
-	return head;
+    if (array == NULL || size <= 0)
+    {
+        return NULL;
+    }
+    Node* head = new Node;
+    Node* p = head;
+    for (int i = 0; i < size; ++i)
+    {
+        p->next = new Node;
+        p->next->value = array[i];
+        p = p->next;
+    }
+    return head;
 }
 
 Node* merge(Node* head1, Node* head2)
 {
-	if (head1 == NULL || head2 == NULL || head1->next == NULL || head2->next == NULL)
-	{
-		return NULL;
-	}
-	Node* head = head1->next->value < head2->next->value ? head1 : head2;
-	Node* p1 = head1->next;
-	Node* p2 = head2->next;
-	Node* p = head;
-	while (p1 && p2)
-	{
-	    if (p1->value < p2->value)
-	    {
-	    	p->next = p1;
-	    	p = p1;
-	    	p1 = p1->next;
-	    }
-	    else
-	    {
-	        p->next = p2;
-	        p = p2;
-	        p2 = p2->next;
-	    }
-	}
-	if (p1 == NULL)
-	{
-		p->next = p2;
-	}
-	else
-	{
-	    p->next = p1;
-	}
-	if (head == head1)
-	{
-		delete head2;
-	}
-	else
-	{
-	    delete head1;
-	}
-	return head;
+    if (head1 == NULL || head2 == NULL || head1->next == NULL || head2->next == NULL)
+    {
+        return NULL;
+    }
+    Node* head = head1->next->value < head2->next->value ? head1 : head2;
+    Node* p1 = head1->next;
+    Node* p2 = head2->next;
+    Node* p = head;
+    while (p1 && p2)
+    {
+        if (p1->value < p2->value)
+        {
+            p->next = p1;
+            p = p1;
+            p1 = p1->next;
+        }
+        else
+        {
+            p->next = p2;
+            p = p2;
+            p2 = p2->next;
+        }
+    }
+    if (p1 == NULL)
+    {
+        p->next = p2;
+    }
+    else
+    {
+        p->next = p1;
+    }
+    if (head == head1)
+    {
+        delete head2;
+    }
+    else
+    {
+        delete head1;
+    }
+    return head;
 }
 
 int main(int argc, char* argv[])
 {
-	{
-		int array1[] = {1, 4, 5, 7, 10};
-		int array2[] = {2, 3, 6, 8, 9};
-		Node* head1 = initList(array1, 5);
-		Node* head2 = initList(array2, 5);
-		Node* head3 = merge(head1, head2);
-		for (Node* p = head3; p->next; p = p->next)
-		{
-			printf("%d ", p->next->value);
-		}
-		printf("\n");
-	}
+    {
+        int array1[] = {1, 4, 5, 7, 10};
+        int array2[] = {2, 3, 6, 8, 9};
+        Node* head1 = initList(array1, 5);
+        Node* head2 = initList(array2, 5);
+        Node* head3 = merge(head1, head2);
+        for (Node* p = head3; p->next; p = p->next)
+        {
+            printf("%d ", p->next->value);
+        }
+        printf("\n");
+    }
 
-	{
-		int array1[] = {1, 2, 3, 4, 5};
-		int array2[] = {6, 7, 8, 9, 10};
-		Node* head1 = initList(array1, 5);
-		Node* head2 = initList(array2, 5);
-		Node* head3 = merge(head1, head2);
-		for (Node* p = head3; p->next; p = p->next)
-		{
-			printf("%d ", p->next->value);
-		}
-		printf("\n");
-	}
+    {
+        int array1[] = {1, 2, 3, 4, 5};
+        int array2[] = {6, 7, 8, 9, 10};
+        Node* head1 = initList(array1, 5);
+        Node* head2 = initList(array2, 5);
+        Node* head3 = merge(head1, head2);
+        for (Node* p = head3; p->next; p = p->next)
+        {
+            printf("%d ", p->next->value);
+        }
+        printf("\n");
+    }
 
-	{
-		int array1[] = {1, 1, 1, 1, 1};
-		int array2[] = {1, 1, 1, 1, 1};
-		Node* head1 = initList(array1, 5);
-		Node* head2 = initList(array2, 5);
-		Node* head3 = merge(head1, head2);
-		for (Node* p = head3; p->next; p = p->next)
-		{
-			printf("%d ", p->next->value);
-		}
-		printf("\n");
-	}
+    {
+        int array1[] = {1, 1, 1, 1, 1};
+        int array2[] = {1, 1, 1, 1, 1};
+        Node* head1 = initList(array1, 5);
+        Node* head2 = initList(array2, 5);
+        Node* head3 = merge(head1, head2);
+        for (Node* p = head3; p->next; p = p->next)
+        {
+            printf("%d ", p->next->value);
+        }
+        printf("\n");
+    }
 
-	{
-		assert(merge(NULL, NULL) == NULL);
-	}
-	return 0;
+    {
+        assert(merge(NULL, NULL) == NULL);
+    }
+    return 0;
 }
